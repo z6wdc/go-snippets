c4e8e683859a30077bde193e5a19935b8ea2a083
z6wdc <whitedeathcross@gmail.com>
2022-02-03 13:38:13 +0800

    http connection sample

---
+++ http/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"net/http"
+)
+
+type logwriter struct {
+}
+
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	lw := logwriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logwriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+
+	return len(bs), nil
+}
