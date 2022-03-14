f6c0b1144f1eda5eb0dbdf42c91d9fab764aabf7
z6wdc <whitedeathcross@gmail.com>
2022-03-14 17:39:54 +0900

    ex 4

--- learngo/06-variables/04-assignment/exercises/04-find-the-rectangle-perimeter/main.go
+++ learngo/06-variables/04-assignment/exercises/04-find-the-rectangle-perimeter/main.go
@@ -8,6 +8,8 @@
 
 package main
 
+import "fmt"
+
 // ---------------------------------------------------------
 // EXERCISE: Find the Rectangle's Perimeter
 //
@@ -31,14 +33,12 @@ package main
 // ---------------------------------------------------------
 
 func main() {
-	// UNCOMMENT THE CODE BELOW:
 
-	// var (
-	// 	perimeter        int
-	// 	width, height = 5, 6
-	// )
+	var (
+		perimeter     int
+		width, height = 5, 6
+	)
 
-	// USE THE VARIABLES ABOVE WHEN CALCULATING YOUR RESULT
-
-	// ADD YOUR CODE BELOW
+	perimeter = 2 * (width + height)
+	fmt.Println(perimeter)
 }
